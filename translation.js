f49344f41e165c5107fe3106338bb0aa58d4d7c2
Eduardo Viana Pessoa <eduardo.vianapessoa@vivint.com>
2024-10-22 17:20:37 -0600

    Desenvolvimento inicial do portfólio

---
+++ translation.js
@@ -0,0 +1,10 @@
+//0 = PT, 1 = EN, 2 = ES
+let language = 1;
+
+let title = [
+    'Eu sou Eduardo Pessoa',
+    'I am Eduardo Pessoa',
+    'Yo soy Eduardo Pessoa'
+];
+
+console.log(title[language]);
